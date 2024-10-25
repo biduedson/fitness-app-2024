b5aca0aac2014cb88acda492829ae0897de1d03e
Edson <biduzao.bidu21@gmail.com>
2024-10-24 23:18:48 -0300

    feat:remove component login

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,8 @@ import Hero from "@/components/Hero";
 import Membership from "@/components/Membership";
 import Team from "@/components/Team";
 import Testimonial from "@/components/Testimonial";
-import { db } from "./_lib/prisma";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Login from "@/components/Login";
 export default async function Home() {
   return (
     <main>
@@ -23,7 +21,6 @@ export default async function Home() {
       <Testimonial />
       <Blog />
       <Brands />
-      <Login />
 
       {/*temporary div*/}
       {/*<div className='h-[3000px]'></div>*/}
