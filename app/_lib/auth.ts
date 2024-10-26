082c0cbf5ce308ee475041c262fb80c9dd13f9b7
Edson <biduzao.bidu21@gmail.com>
2024-10-26 15:16:45 -0300

    feat: new confi next auth

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,59 +1,68 @@
-import { db } from "@/app/_lib/prisma"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import NextAuth, { AuthOptions } from "next-auth"
+import { db } from "@/app/_lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 import InstagramProvider from "next-auth/providers/instagram";
-
-
-import { Adapter } from "next-auth/adapters"
-import GoogleProvider from 'next-auth/providers/google'
-import { GymAdmin, Student } from "@prisma/client";
+import GoogleProvider from "next-auth/providers/google";
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(db) as Adapter,
-    providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-        FacebookProvider({
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
-     InstagramProvider({
-    clientId: process.env.INSTAGRAM_CLIENT_ID as string,
-    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET as string ,
-    
-  })
-    ],
-    
-     session: {
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID as string,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET as string,
+    }),
+  ],
+  
+  session: {
     strategy: "database",
   },
-
-   callbacks: {
+  
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      // Aqui você pode adicionar lógica de verificação, se necessário
+      return true; // Permite o login
+    },
+    
     async session({ session, user }) {
-      if (!user){
-         return session;
-        } // Retorna a sessão sem modificação se `user` estiver ausente.
+      if (!user) {
+        return session;
+      } // Retorna a sessão sem modificação se `user` estiver ausente.
+
       const userWithDetails = await db.user.findUnique({
         where: { id: user.id },
         include: { student: true, gymAdmin: true },
       });
 
-     if (userWithDetails) {
+      if (userWithDetails) {
         session.user = {
           ...session.user,
-          id: userWithDetails.id as string ,
+          id: userWithDetails.id,
           student: userWithDetails.student || null,
           gymAdmin: userWithDetails.gymAdmin || null,
         };
       } else {
         console.warn("User details not found for user id:", user.id);
       }
-console.log("Session User:", session.user); // Debugging linha
+
+      console.log("Session User:", session.user); // Debugging linha
       return session;
     },
   },
+
   secret: process.env.NEXTAUTH_SECRET,
-}
+  
+  // Ativa o modo debug para mais informações de log
+  debug: true,
+};
+
+export default NextAuth(authOptions);
