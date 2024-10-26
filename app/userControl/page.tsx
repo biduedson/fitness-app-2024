10c49898b1c19d17197decb67f96eb1d6061791a
Edson <biduzao.bidu21@gmail.com>
2024-10-26 14:30:42 -0300

    feat: remove  redirect login

--- app/userControl/page.tsx
+++ app/userControl/page.tsx
@@ -6,6 +6,8 @@ import CardUserDashbord from "./components/CardUserDashbord";
 import { notFound, redirect, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { Prisma, User } from "@prisma/client";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { GoBlocked } from "react-icons/go";
 
 interface IUsers {
   users: Prisma.UserGetPayload<{
@@ -20,15 +22,17 @@ const fetcher = (url: string): Promise<IUsers> =>
   fetch(url).then((res) => res.json());
 
 const page = () => {
-  const router = useRouter();
   const { data: session, status } = useSession();
 
-  if (!session?.user) {
-    return router.push("/login");
-  }
-
   if (!session?.user.gymAdmin) {
-    return notFound();
+    return (
+      <div className=" w-full h-[100vh] flex gap-1 items-center justify-center bg-black_texture text-white ">
+        <span className="animate-pulse text-[20px]">
+          <GoBlocked />
+        </span>
+        <span>Loading...</span>
+      </div>
+    );
   }
 
   const { data: initialUser, error } = useSWR<IUsers>(() => {
@@ -46,7 +50,23 @@ const page = () => {
       console.log(initialUser);
     }
   }, [initialUser]);
-
+  if (!usersData) {
+    return (
+      <div className=" w-full h-[100vh] flex gap-1 items-center justify-center bg-black_texture text-white ">
+        <span className=" animate-spin text-[20px]">
+          <AiOutlineLoading3Quarters />
+        </span>
+        <span className=" animate-pulse">Loading...</span>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className=" w-full h-[100vh] flex gap-1 items-center justify-center bg-black_texture text-white ">
+        <span className="text-[20px]">Erro: {error}</span>
+      </div>
+    );
+  }
   if (usersData?.users?.length === 0) {
     return (
       <div className=" w-full h-[100vh] text-white flex justify-center items-center ">
