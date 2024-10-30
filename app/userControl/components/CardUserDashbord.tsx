d03f4a55d52f2ec25523fae086d721b293e85d01
Edson <biduzao.bidu21@gmail.com>
2024-10-30 13:59:24 -0300

    chore: size down

--- app/userControl/components/CardUserDashbord.tsx
+++ app/userControl/components/CardUserDashbord.tsx
@@ -97,8 +97,8 @@ const CardUserDashbord = ({ users }: ICardUserDashbordProps) => {
           className="h2 flex flex-col   w-full sm:w-[380px] sm:text-[40px]
              text-center  text-accent le leading-[1.1]  border-white border-b-[1px] "
         >
-          <span className="text-[50px] sm:text-[70px] mb-2">Controle</span>
-          <span className="text-[30px] sm:text-[40px] mb-2">de usuários</span>
+          <span className="text-[40px] sm:text-[60px] mb-2">Controle</span>
+          <span className="text-[20px] sm:text-[30px] mb-2">de usuários</span>
         </motion.h2>
         <SearchUser handleInputChange={handleInputChange} />
         <CardUserList users={filteredUsers} />
